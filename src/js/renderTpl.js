60530a9274544f90698c383f21e13b7b13a6b003
Sergey Prijan <dneprserp355@gmail.com>
2021-11-25 01:59:54 +0200

    create mobile header html

--- src/js/renderTpl.js
+++ src/js/renderTpl.js
@@ -1,6 +1,2 @@
-import header from '../html/parts/header.html'
-import footer from '../html/parts/footer.html'
-document.querySelector('.header').innerHTML = header
-document.querySelector('.footer').innerHTML = footer
-
-// renderFooter()
+import header from '../html/parts/header.html';
+document.querySelector('.header').innerHTML = header;
