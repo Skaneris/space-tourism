0cd033d587fdb22819ab33d89f5eaeec1b7ab533
Sergey Prijan <dneprserp355@gmail.com>
2021-11-25 04:48:12 +0200

    complete mobile home

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './js/renderTpl'
+import './js/burger'
 // import './js/script'
-// import './js/burger'
 import './sass/app.sass'
 
 // console.log(document.location);
--- src/js/burger.js
+++ src/js/burger.js
@@ -3,28 +3,9 @@ const burgerMenu = () => {
     const menu = document.querySelector('.header__menu')
     const close = document.querySelector('.btn__close')
 
-    const mobileMenuClose = (overlay) => {
-        overlay.classList.remove('_active')
-        menu.classList.remove('_active')
-        setTimeout(() => overlay.remove(), 600)
-    }
-
-    burger.addEventListener('click', () => {
-        menu.classList.add('_active')
-        const overlay = document.createElement("div")
-        overlay.classList.add('overlay')
-        document.body.append(overlay)
-        setTimeout(() => overlay.classList.add('_active'), 0)
-        
-        overlay.onclick = () => {
-            mobileMenuClose(overlay)
-        }
-    })
-
-    close.addEventListener('click', () => {
-        const overlay = document.querySelector('.overlay')
-        mobileMenuClose(overlay)
-    })
+    const toggleMenu = ev => menu.classList.toggle('_active')
+    burger.addEventListener('click', toggleMenu)
+    close.addEventListener('click', toggleMenu)
 }
 
 burgerMenu()
